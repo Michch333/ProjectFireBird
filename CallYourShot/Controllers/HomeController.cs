f4bd8385510e7cc8c49b7a64725d4b8404e27cd9
Jason Walser <r0flcopt0rz10611@gmail.com>
2021-10-01 00:04:41 -0400

    worked on winners logic

--- CallYourShot/Controllers/HomeController.cs
+++ CallYourShot/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CallYourShot.Controllers
 {
@@ -42,35 +41,66 @@ namespace CallYourShot.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public int DiceRoll() 
+        //public int DiceRoll() 
+        //{
+        //        Random dice = new Random();
+        //        int roll = dice.Next(1, 60);
+        //        return roll;
+        //}
+        public Player DiceRoll(Player player)
         {
-                Random dice = new Random();
-                int roll = dice.Next(1, 60);
-                return roll;
+            Random dice = new Random();
+            player.Roll = dice.Next(1, 60);
+            return player;
         }
         public IActionResult GameRoll(List<Player> players, int numberToRollUnder)
         {
-
             var playersWithRolls = new List<Player>();
-            foreach(var player in players)
+
+            foreach (var player in players)
             {
-                int playerRoll =DiceRoll();
-                if(playerRoll <= numberToRollUnder)
+                DiceRoll(player);
+
+                if (player.Roll <= numberToRollUnder)
                 {
-                    player.Roll = playerRoll;
                     playersWithRolls.Add(player);
                 }
-             }
-            if (playersWithRolls.Count() == 0)
-            {
-                GameRoll( players, numberToRollUnder);
             }
 
-            var orderdedList = playersWithRolls.OrderBy(p => p.Roll).ToList<Player>();
+            CheckforWinner(players, numberToRollUnder, playersWithRolls);
             var viewModel = new RollInfoViewModel();
-            viewModel.RollWinner = orderdedList[0];
+            viewModel.RollWinner = playersWithRolls.FirstOrDefault();
             return View("Index", viewModel);
         }
-        
+
+        private void CheckforWinner(List<Player> players, int numberToRollUnder, List<Player> playersWithRolls)
+        {
+            if (playersWithRolls.Count() == 0)
+            {
+                GameRoll(players, numberToRollUnder);
+            }
+
+            if (playersWithRolls.Count() >= 2)
+            {
+                CheckForTie(playersWithRolls, numberToRollUnder);
+            }
+        }
+
+        public void CheckForTie(List<Player> playersWithRolls,int numberToRollUnder)
+        {
+            var tiedPLayersList = new List<Player>();
+            var orderdedList = playersWithRolls.OrderBy(p => p.Roll).ToList<Player>();
+
+            foreach (Player player in orderdedList)
+            {
+                if (player.Roll == orderdedList.FirstOrDefault().Roll)
+                {
+                    tiedPLayersList.Add(player);
+                }
+
+            }
+            GameRoll(tiedPLayersList, numberToRollUnder);
+        }
+
     }
 }
